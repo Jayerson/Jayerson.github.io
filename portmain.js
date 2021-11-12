1999d2dd5342bb85ec0ad7fc4f66d1a4a1142847
Jayerson <ayersonj@gmail.com>
2021-11-12 16:11:33 +1100

    Boilerplate

---
+++ portmain.js
@@ -0,0 +1,17 @@
+function showTopics() {
+    var x = document.getElementById("collapsible");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function showEng() {
+    var x = document.getElementById("be-hidden");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
