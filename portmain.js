2df3921af394416cec2852624124064c84056566
Jayerson <ayersonj@gmail.com>
2021-11-23 13:23:10 +1100

    placeholders and styling update

--- portmain.js
+++ portmain.js
@@ -1,9 +1,12 @@
 function showTopics() {
     var x = document.getElementById("collapsible");
+    var y = document.getElementById("swapOut");
     if (x.style.display === "block") {
       x.style.display = "none";
+      y.style.display = "block";
     } else {
       x.style.display = "block";
+      y.style.display = "none";
     }
   }
 
