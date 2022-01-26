5cf86b949a2d4e59acb87231b203c2b3e4ae4e0d
Jayerson <ayersonj@gmail.com>
2022-01-26 22:50:21 +1100

    final week update

--- portmain.js
+++ portmain.js
@@ -1,6 +1,21 @@
 function showTopics() {
+    var w = document.getElementById("about_inline");
     var x = document.getElementById("collapsible");
-    var y = document.getElementById("swapOut");
+    var y = document.getElementById("swapout");
+    if (x.style.display === "block") {
+      w.style.display = "block";
+      x.style.display = "none";
+      y.style.display = "block";
+    } else {
+      w.style.display = "none";
+      x.style.display = "block";
+      y.style.display = "none";
+    }
+}
+
+function showEng() {
+    var x = document.getElementById("be-hidden");
+    var y = document.getElementById("rocket");
     if (x.style.display === "block") {
       x.style.display = "none";
       y.style.display = "block";
@@ -8,13 +23,14 @@ function showTopics() {
       x.style.display = "block";
       y.style.display = "none";
     }
-  }
+}
 
-function showEng() {
-    var x = document.getElementById("be-hidden");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+window.onscroll = function() {stick()};
+
+var navBar = document.getElementById("sticky_nav");
+var sticky = navBar.offsetTop;
+function stick() {
+  if (window.pageYOffset >= sticky)
+    navBar.classList.add("sticky");
+  else navBar.classList.remove("sticky");
 }
